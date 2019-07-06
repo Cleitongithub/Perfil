705d42250aeb7b1c55a6e6e51f475dc796ad0d75
Cleiton Garcia <cleitongarciapalma@gmail.com>
2019-07-06 16:37:49 -0300

    primeiro commit

---
+++ Exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exemplo {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("entre com seu nome: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("entre com sua idade");
+            int b = int.Parse(Console.ReadLine());
+
+            if ( b >= 18){
+                Console.WriteLine("é maior de idade ");
+            }
+            else {
+                Console.WriteLine("menor de idade ");
+            }
+                                   
+
+        }
+    }
+}
